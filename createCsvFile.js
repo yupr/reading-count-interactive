2080d720fe5c832659d8f0155dc7f180c880dcab
yupr <12003186+yupr@users.noreply.github.com>
2021-09-24 23:38:55 +0900

    指定した月の日付が入ったcsvファイルを書き出す機能を実装

---
+++ createCsvFile.js
@@ -0,0 +1,36 @@
+const fs = require('fs'); 
+const stringify = require('csv-stringify')
+
+//作成したい日時と文字数の初期値
+const initialValue = [{ date: 211001, count: 0}]
+// let monthInitialValue = [];
+
+//その月分の日付と初期値が入力された配列オブジェクトを作成
+for(let i=1; i<31; i++){
+  let data = { date :initialValue[initialValue.length -1].date + 1, count: 0}
+  initialValue.push(data)
+}
+
+//配列オブジェクトをcsvに変換
+const outputCsv = (() =>{
+  return new Promise((resolve, reject) =>{
+    stringify(initialValue, {
+      header: true
+    }, (err, output) =>{
+      if(err){
+        reject(err)
+        return;
+      }
+      resolve(output)
+    })
+  })
+})
+
+//変換されたcsvを新規ファイルとして作成
+const createNewFile = (async() =>{
+  const output = await outputCsv();
+  fs.writeFile('2110.csv', output, (err) =>{
+    if(err) console.log('err', err)
+  })
+}) 
+createNewFile()
--- getTotal.js
+++ getTotal.js
@@ -1,15 +1,31 @@
 const fs = require('fs');
-const parse = require('csv-parse/lib/sync')
+const parse = require('csv-parse')
+//同期処理の場合: const parse = require('csv-parse/lib/sync')
 
-const data = fs.readFileSync('./2110.csv');
-const result = parse(data, {
-  columns: true,
-})
-console.log('result', result)
-
-let totalCount = 0;
-for(let i=0; i<result.length; i++){
-  const wordCount = Number(result[i].count)
-  totalCount += wordCount;
+//csvファイルを配列オブジェクトに変換
+const outputArray = () => {
+  return new Promise((resolve, reject) =>{
+    const fileData = fs.readFileSync('./2110.csv');
+    parse(fileData, {
+      columns: true,
+    }, (err, output) =>{
+      if(err){
+        reject(err)
+        return;
+      }
+      resolve(output)
+    })
+  })
 }
-console.log('totalCount', totalCount)
+
+//変換されたオブジェクトからcountのtotalを集計
+const getTotalCount = async() =>{
+  const result = await outputArray();
+  let totalCount = 0;
+  for(let i=0; i<result.length; i++){
+    const wordCount = Number(result[i].count)
+    totalCount += wordCount;
+  }
+  console.log('計:', totalCount)  
+}
+getTotalCount()
