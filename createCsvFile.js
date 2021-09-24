caa7f9701e1f96128e8174e9c8d0cb343f95e02d
yupr <12003186+yupr@users.noreply.github.com>
2021-09-25 03:16:18 +0900

    fix

--- createCsvFile.js
+++ createCsvFile.js
@@ -1,22 +1,28 @@
-const fs = require('fs'); 
+const fs = require('fs').promises;
 const stringify = require('csv-stringify')
 
 //作成したい日時と文字数の初期値
-const initialValue = [{ date: 211001, count: 0}]
+const initialValue = [{
+  date: 211001,
+  count: 0
+}]
 
 //その月分の日付と初期値が入力された配列オブジェクトを作成
-for(let i=1; i<31; i++){
-  let data = { date :initialValue[initialValue.length -1].date + 1, count: 0}
+for (let i = 1; i < 31; i++) {
+  let data = {
+    date: initialValue[initialValue.length - 1].date + 1,
+    count: 0
+  }
   initialValue.push(data)
 }
 
 //配列オブジェクトをcsvに変換
-const outputCsv = (() =>{
-  return new Promise((resolve, reject) =>{
+const outputCsv = (() => {
+  return new Promise((resolve, reject) => {
     stringify(initialValue, {
       header: true
-    }, (err, output) =>{
-      if(err){
+    }, (err, output) => {
+      if (err) {
         reject(err)
         return;
       }
@@ -25,17 +31,32 @@ const outputCsv = (() =>{
   })
 })
 
-//変換されたcsvを新規ファイルで作成
-const createNewFile = (async() =>{
-  const isExistFile = !!(fs.readFileSync('./2109.csv'));
-
-  //ファイルが作られていなかったら実行
-  if(!isExistFile){
-    const output = !isExistFile && await outputCsv();
-    //書き出したいファイル名を指定
-    fs.writeFile('2110.csv', output, (err) =>{
-      if(err) console.log('err', err)
-    })
+//ファイルの存在チェック
+const isFileExist = async () => {
+  let isExist = false;
+  try {
+    await fs.readFile('./2108.csv')
+    isExist = true;
+  } catch (err) {
+    isExist = false;
   }
-}) 
+  return isExist;
+}
+
+//変換されたcsvを新規ファイルで作成
+const createNewFile = (async () => {
+  const isFile = await isFileExist();
+
+  //ファイルが存在しなかったら新規作成
+  if (!isFile) {
+    const output = await outputCsv();
+    console.log('output', output)
+
+    if (output) {
+      //書き出したいファイル名を指定
+      await fs.writeFile('2110.csv', output)
+    }
+  }
+})
+
 createNewFile()
