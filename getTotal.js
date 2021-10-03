f3f1655c2f865b0de38eee3058ae2e3b5c560a90
yupr <12003186+yupr@users.noreply.github.com>
2021-10-03 19:20:28 +0900

    コマンドライン引数をファイルに渡す

--- getTotal.js
+++ getTotal.js
@@ -1,12 +1,19 @@
 const fs = require('fs').promises
 const parse = require('csv-parse')
+const program = require('commander')
+
+//コマンドライン引数をcommanderでパース
+program.parse(process.argv);
+
+//ファイルパスをprocess.args配列から取り出す
+const filePath = program.args[0]
 
 //csvファイルを配列オブジェクトに変換
 const outputArray = () => {
   return new Promise(async (resolve, reject) => {
     //集計したいファイル名を指定
     try {
-      const file = await fs.readFile('./Archive/2021/2109.csv')
+      const file = await fs.readFile(filePath)
       parse(file, {
         columns: true,
       }, (err, output) => {
