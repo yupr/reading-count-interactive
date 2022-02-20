ffd46ba826d899f4baafea8bdd8ee6b497b4f2ba
yupr <12003186+yupr@users.noreply.github.com>
2022-02-20 22:35:13 +0900

    prettier: include eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,14 +4,14 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "prettier"],
+  extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 13,
-    "sourceType": "module"
+    sourceType: 'module',
   },
   rules: {
-    "no-console": "off", //consoleを許容
-    "no-var": "error", // varは禁止
-    "no-extra-semi": "warn", //余分なセミコロンは禁止(文末に ;; とか)
+    'no-console': 'off', //consoleを許容
+    'no-var': 'error', // varは禁止
+    'no-extra-semi': 'warn', //余分なセミコロンは禁止(文末に ;; とか)
   },
 };
