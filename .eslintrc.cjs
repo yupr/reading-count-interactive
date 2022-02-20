2ee7fbb4a07ac22a87f5cdaeae7456a41018ffe0
yupr <12003186+yupr@users.noreply.github.com>
2022-02-20 22:55:01 +0900

    eslint add plugin: unused-imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
+  plugins: ['unused-imports'],
   extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 13,
@@ -11,6 +12,7 @@ module.exports = {
   },
   rules: {
     'no-console': 'off', //consoleを許容
+    "unused-imports/no-unused-imports": "error",
     'no-var': 'error', // varは禁止
     'no-extra-semi': 'warn', //余分なセミコロンは禁止(文末に ;; とか)
   },
